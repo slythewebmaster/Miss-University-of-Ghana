d363ee9e8bac8904cf40833abcbbfbd9ca1f70c1
slythewebmaster <sylvesterofori577@gmail.com>
2025-06-09 19:30:17 +0000

    feat: add additional sponsor logos to the SponsorSection component

--- miss-ug/src/components/SponsorSection/SponsorSection.jsx
+++ miss-ug/src/components/SponsorSection/SponsorSection.jsx
@@ -7,6 +7,8 @@ const sponsors = [
   { img: "https://i.pinimg.com/736x/cf/56/7e/cf567e10b5a03533207799da995b1954.jpg" },
   { img: "https://i.pinimg.com/736x/08/10/0b/08100b6c37339eefc10a5175de62a42e.jpg" },
   { img: "https://i.pinimg.com/736x/39/72/a2/3972a24854006ceadd4bb970cfb3921b.jpg" },
+  { img: "https://i.pinimg.com/736x/14/af/c4/14afc4773353129def6920981239c4eb.jpg" },
+  { img: "https://i.pinimg.com/736x/de/c1/32/dec1323ea7ef54e926653b980395eef3.jpg" },
   { img: "https://i.pinimg.com/736x/8b/4f/07/8b4f07f562e4c3c83e10e3f2471a3f5b.jpg" },
   { img: "https://i.pinimg.com/736x/ae/fa/d8/aefad881ad1995e5150c170da5daa666.jpg" },
 ];
